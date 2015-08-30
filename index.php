5aa97c842ad9de1a8acd5621c94ba10a9f8ccef1
Maksim Klimenko <klym.programmer@yandex.ru>
2015-08-30 11:28:56 +0300

    add site structure

--- index.php
+++ index.php
@@ -3,9 +3,18 @@
 <head>
 <meta charset="utf-8">
 <link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
 <title>Союз Предпринимателей ДНР</title>
 </head>
-
 <body>
+	<div id="wrapper">
+    	<header></header>
+        <nav></nav>
+        <section></section>
+        <div id="partners"></div>
+        <div id="about"></div>
+        <div id="reg"></div>
+        <footer></footer>
+    </div>
 </body>
 </html>
