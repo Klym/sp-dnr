88471cbfbf60e473358cc86f186d3f94ebc4b97b
Maksim Klimenko <klym.programmer@yandex.ru>
2015-08-30 13:44:40 +0300

    add header and article styles

--- index.php
+++ index.php
@@ -8,9 +8,53 @@
 </head>
 <body>
 	<div id="wrapper">
-    	<header></header>
+    	<header>
+        	<div id="logo"></div>
+            <div id="search"></div>
+        </header>
         <nav></nav>
-        <section></section>
+        <section>
+        	<article>
+            	<header>Новости союза</header>
+                <div class="articleBody">
+                	<p><img src="img/articleLogo.jpg" width="123" height="98" alt="Логотип статьи">
+                    <div class="articleTitle">Заголовок новости</div>
+                    <div class="articleDate">26,11,2015</div>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,sem. Nulla consequat massa quis enim. <a href="#">Читать далее...</a></p>
+                </div>
+                <footer><a href="#">Перейти ко всем новостям союза</a></footer>
+            </article>
+            <article>
+            	<header>Мероприятия</header>
+                <div class="articleBody">
+                	<p><img src="img/articleLogo.jpg" width="123" height="98" alt="Логотип статьи">
+                    <div class="articleTitle">Заголовок новости</div>
+                    <div class="articleDate">26,11,2015</div>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,sem. Nulla consequat massa quis enim. <a href="#">Читать далее...</a></p>
+                </div>
+                <footer><a href="#">Перейти ко всем мероприятиям</a></footer>
+            </article>
+            <article>
+            <header>Новости финансов</header>
+                <div class="articleBody">
+                	<p><img src="img/articleLogo.jpg" width="123" height="98" alt="Логотип статьи">
+                    <div class="articleTitle">Заголовок новости</div>
+                    <div class="articleDate">26,11,2015</div>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,sem. Nulla consequat massa quis enim. <a href="#">Читать далее...</a></p>
+                </div>
+                <footer><a href="#">Перейти ко всем новостям финансов</a></footer>
+            </article>
+            <article>
+            	<header>Новости чего-то</header>
+                <div class="articleBody">
+                	<p><img src="img/articleLogo.jpg" width="123" height="98" alt="Логотип статьи">
+                    <div class="articleTitle">Заголовок новости</div>
+                    <div class="articleDate">26,11,2015</div>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,sem. Nulla consequat massa quis enim. <a href="#">Читать далее...</a></p>
+                </div>
+                <footer><a href="#">Перейти ко всем новостям чего-то</a></footer>
+            </article>
+        </section>
         <div id="partners"></div>
         <div id="about"></div>
         <div id="reg"></div>
