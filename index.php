fa258c18f36695f7dea7a473722e796cea50cd22
Maksim Klimenko <klym.programmer@yandex.ru>
2015-08-30 15:58:53 +0300

    add footer and paragraph styles

--- index.php
+++ index.php
@@ -17,42 +17,42 @@
         	<article>
             	<header>Новости союза</header>
                 <div class="articleBody">
-                	<p><img src="img/articleLogo.jpg" width="123" height="98" alt="Логотип статьи">
-                    <div class="articleTitle">Заголовок новости</div>
-                    <div class="articleDate">26,11,2015</div>
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,sem. Nulla consequat massa quis enim. <a href="#">Читать далее...</a></p>
+                	<p><img src="img/articleLogo.jpg" width="123" height="98" alt="Логотип статьи"></p>
+                    <div class="articleTitle"><p>Заголовок новости</p></div>
+                    <div class="articleDate"><p>26,11,2015</p></div>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,sem. Nulla consequat massa quis enim. <a href="#">Читать далее...</a></p>
                 </div>
-                <footer><a href="#">Перейти ко всем новостям союза</a></footer>
+                <footer><p><a href="#">Перейти ко всем новостям союза</a></p></footer>
             </article>
             <article>
             	<header>Мероприятия</header>
                 <div class="articleBody">
-                	<p><img src="img/articleLogo.jpg" width="123" height="98" alt="Логотип статьи">
-                    <div class="articleTitle">Заголовок новости</div>
-                    <div class="articleDate">26,11,2015</div>
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,sem. Nulla consequat massa quis enim. <a href="#">Читать далее...</a></p>
+                	<p><img src="img/articleLogo.jpg" width="123" height="98" alt="Логотип статьи"></p>
+                    <div class="articleTitle"><p>Заголовок новости</p></div>
+                    <div class="articleDate"><p>26,11,2015</p></div>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,sem. Nulla consequat massa quis enim. <a href="#">Читать далее...</a></p>
                 </div>
-                <footer><a href="#">Перейти ко всем мероприятиям</a></footer>
+                <footer><p><a href="#">Перейти ко всем мероприятиям</a></p></footer>
             </article>
             <article>
             <header>Новости финансов</header>
                 <div class="articleBody">
-                	<p><img src="img/articleLogo.jpg" width="123" height="98" alt="Логотип статьи">
-                    <div class="articleTitle">Заголовок новости</div>
-                    <div class="articleDate">26,11,2015</div>
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,sem. Nulla consequat massa quis enim. <a href="#">Читать далее...</a></p>
+                	<p><img src="img/articleLogo.jpg" width="123" height="98" alt="Логотип статьи"></p>
+                    <div class="articleTitle"><p>Заголовок новости</p></div>
+                    <div class="articleDate"><p>26,11,2015</p></div>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,sem. Nulla consequat massa quis enim. <a href="#">Читать далее...</a></p>
                 </div>
-                <footer><a href="#">Перейти ко всем новостям финансов</a></footer>
+                <footer><p><a href="#">Перейти ко всем новостям финансов</a></p></footer>
             </article>
             <article>
             	<header>Новости чего-то</header>
                 <div class="articleBody">
-                	<p><img src="img/articleLogo.jpg" width="123" height="98" alt="Логотип статьи">
-                    <div class="articleTitle">Заголовок новости</div>
-                    <div class="articleDate">26,11,2015</div>
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,sem. Nulla consequat massa quis enim. <a href="#">Читать далее...</a></p>
+                	<p><img src="img/articleLogo.jpg" width="123" height="98" alt="Логотип статьи"></p>
+                    <div class="articleTitle"><p>Заголовок новости</p></div>
+                    <div class="articleDate"><p>26,11,2015</p></div>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,sem. Nulla consequat massa quis enim. <a href="#">Читать далее...</a></p>
                 </div>
-                <footer><a href="#">Перейти ко всем новостям чего-то</a></footer>
+                <footer><p><a href="#">Перейти ко всем новостям чего-то</a></p></footer>
             </article>
         </section>
         <div id="partners">
@@ -71,7 +71,20 @@
             	<span class="title">Стать членом союза »</span>
 			</header>
         </div>
-        <footer></footer>
+        <footer>
+        	<div id="dnr-big-flag"></div>
+            <div id="copyright">
+            	<p>© Союз Предпринимателей ДНР<br>
+                Использование любых материалов,<br>
+                размещённых на данном сайте,<br>
+                разрешается при условии ссылки<br>
+                на sp-dnr.ru</p>
+            </div>
+            <address>
+                <p>Адрес: ул. Лермонтова 25, Донецк, ДНР<br>
+                Телефон: 071-356-22-22</p>
+            </address>
+        </footer>
     </div>
 </body>
 </html>
