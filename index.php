9b9bce1eda3b85e1ff964e31a88f5bf249f641df
Maksim Klimenko <klym.programmer@yandex.ru>
2015-08-30 10:55:16 +0300

    initialize repository

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<title>Союз Предпринимателей ДНР</title>
+</head>
+
+<body>
+</body>
+</html>
