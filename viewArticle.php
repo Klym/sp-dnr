f51afa7551dda0f7edab9f364304222532fb7db9
Maksim Klimenko <klym.programmer@yandex.ru>
2015-09-03 00:27:51 +0300

    add breadcrumbs styles

---
+++ viewArticle.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script src="js/scripts.js"></script>
+<script src="js/Partners.js"></script>
+<title>Союз Предпринимателей ДНР - Новости</title>
+</head>
+<body>
+	<div id="wrapper" class="news">
+    	<header>
+        	<div id="logo"></div>
+            <div id="search">
+            	<form method="get" name="search"> 
+                    <input type="search" placeholder="Поиск" name="keywords">
+                    <input type="submit" value="">
+				</form>
+            </div>
+        </header>
+        <nav></nav>
+        <div id="news">
+            <section>
+                <nav>
+                	<ul>
+                        <li>Главная</li>
+                        <li class="active">Новости</li>
+                        <li>Заголовок новости</li>                	
+                    </ul>
+                </nav>
+            </section>
+            <aside>
+                <nav>
+                	<div class="newsNavItem">
+                    	<div class="newsNavItemMark"></div>
+                        <div class="newsNavItemText">Все новости</div>
+                    </div>
+                    <div class="newsNavItem">
+                    	<div class="newsNavItemMark"></div>
+                        <div class="newsNavItemText">Новости союза</div>
+                    </div>
+                    <div class="newsNavItem">
+                    	<div class="newsNavItemMark"></div>
+                        <div class="newsNavItemText">Мероприятия</div>
+                    </div>
+                    <div class="newsNavItem">
+                    	<div class="newsNavItemMark"></div>
+                        <div class="newsNavItemText">Экономика</div>
+                    </div>
+                    <div class="newsNavItem">
+                    	<div class="newsNavItemMark"></div>
+                        <div class="newsNavItemText">Что-то</div>
+                    </div>
+                </nav>
+                <div class="line"></div>
+                    <div class="newsNavItem">
+                    	<div class="newsNavItemMark"></div>
+                        <div class="newsNavItemText">Фильтр</div>
+                    </div>
+            </aside>
+        </div>
+        <footer>
+        	<div id="dnr-big-flag"></div>
+            <div id="copyright">
+            	<p>© Союз Предпринимателей ДНР<br>
+                Использование любых материалов,<br>
+                размещённых на данном сайте,<br>
+                разрешается при условии ссылки<br>
+                на sp-dnr.ru</p>
+            </div>
+            <address>
+                <p>Адрес: ул. Лермонтова 25, Донецк, ДНР<br>
+                Телефон: 071-356-22-22</p>
+            </address>
+        </footer>
+    </div>
+</body>
+</html>
