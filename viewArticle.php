ed571751a138eea7bd3ad51f29ca5e318092c633
Maksim Klimenko <klym.programmer@yandex.ru>
2015-09-04 23:38:24 +0300

    add filter form

--- viewArticle.php
+++ viewArticle.php
@@ -66,10 +66,49 @@
                     </div>
                 </nav>
                 <div class="line"></div>
-                    <div class="newsNavItem">
-                    	<div class="newsNavItemMark"></div>
-                        <div class="newsNavItemText">Фильтр</div>
-                    </div>
+                <div class="newsNavItem">
+                    <div class="newsNavItemMark"></div>
+                    <div class="newsNavItemText">Фильтр</div>
+                </div>
+                <div id="filterForm">
+                    <form method="get" name="filter">
+                    	<p>От:</p>
+                        <select>
+                            <option>1</option>
+                            <option>2</option>
+                        </select>
+                        <select>
+                            <option>Январь</option>
+                            <option>Февраль</option>
+                        </select>
+                        <select>
+                            <option>2015</option>
+                            <option>2014</option>
+                        </select>
+                        <p>До:</p>
+                        <select>
+                            <option>1</option>
+                            <option>2</option>
+                        </select>
+                        <select>
+                            <option>Январь</option>
+                            <option>Февраль</option>
+                        </select>
+                        <select>
+                            <option>2015</option>
+                            <option>2014</option>
+                        </select>
+                        <p><label><input type="checkbox"><span class="checkboxText">Точная дата</span></label></p>
+                        <select>
+                        	<option>Выберите категорию</option>
+                        	<option>Все новости</option>
+                            <option>Новости союза</option>
+                            <option>Мероприятия</option>
+                            <option>Экономика</option>
+                        </select>
+                        <input type="submit" value="Фильтровать">
+                    </form>
+                </div>
             </aside>
         </div>
         <footer>
