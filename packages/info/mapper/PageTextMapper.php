58ab8f581487ced09bfcd6a3cffe394855353f00
Maksim Klimenko <klym.programmer@yandex.ru>
2015-10-30 23:00:11 +0300

    add pageText classes

---
+++ packages/info/mapper/PageTextMapper.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace info\mapper;
+require_once("DataMapper.php");
+
+class PageTextMapper extends DataMapper {
+	function __construct(\PDO $pdo) {
+		$this->pdo = $pdo;
+		$this->selectStmt = $this->pdo->prepare("SELECT * FROM texsts WHERE id = ?");
+	}
+	
+	protected function createObject(array $array) {
+		$obj = new \info\domain\PageText($array["id"], null, $array["text"]);
+		return $obj;
+	}
+	
+	protected function selectStmt() {
+		return $this->selectStmt;
+	}
+	
+	protected function selectAllStmt() {
+		return null;
+	}
+}
+
+?>
