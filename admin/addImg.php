d452c81a0fea663d707b918a626edd7e94e2dbb4
Maksim Klimenko <klym.programmer@yandex.ru>
2015-11-04 02:46:10 +0300

    block image uploading for unlogined users

--- admin/addImg.php
+++ admin/addImg.php
@@ -1,54 +1,57 @@
 <?php
-	if (isset($_POST["upload"]) && !empty($_FILES["img"]["name"])) {
-		$i = 0;
-		$uploadedImgs = array();
-		foreach($_FILES["img"]["type"] as $type) {
-			$isImg = true;
-			switch($type) {
-				case "image/jpeg" :
-					$img = imagecreatefromjpeg($_FILES["img"]["tmp_name"][$i]);
-				break;
-				case "image/gif" :
-					$img = imagecreatefromgif($_FILES["img"]["tmp_name"][$i]);
-				break;
-				case "image/png" :
-					$img = imagecreatefrompng($_FILES["img"]["tmp_name"][$i]);
-				break;
-				default:
-					$isImg = false;
-					echo("Ошибка. Файл ".$_FILES["img"]["name"][$i]." содержит неверный формат.<br>");
-				break;
-			}
-			if (!$isImg) {
-				$i++;
-				continue;
-			}
-			$width = imagesx($img);
-			$height = imagesy($img);
-			if ($width <= 955) {
-				$new_width = $width;
-				$new_height = $height;
-			} else {
-				$new_width = 955;
-				$new_height = floor( $height * ( $new_width / $width));
-			}
-			$tmp_img = imagecreatetruecolor($new_width, $new_height);
-			imagecopyresampled($tmp_img, $img, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
-			$new_path = substr($_FILES["img"]["name"][$i], 0, strrpos($_FILES["img"]["name"][$i], "."));
-			$new_path .= time();
-			if (imagejpeg($tmp_img, "../content_imgs/news_imgs/".$new_path.".jpg", 85)) {
-				array_push($uploadedImgs, $new_path);
-				echo "Изображение ".$_FILES["img"]["name"][$i]." успешно загружено.<br>";
-			}
+session_start();
+include("blocks/check.php");
+
+if (isset($_POST["upload"]) && !empty($_FILES["img"]["name"])) {
+	$i = 0;
+	$uploadedImgs = array();
+	foreach($_FILES["img"]["type"] as $type) {
+		$isImg = true;
+		switch($type) {
+			case "image/jpeg" :
+				$img = imagecreatefromjpeg($_FILES["img"]["tmp_name"][$i]);
+			break;
+			case "image/gif" :
+				$img = imagecreatefromgif($_FILES["img"]["tmp_name"][$i]);
+			break;
+			case "image/png" :
+				$img = imagecreatefrompng($_FILES["img"]["tmp_name"][$i]);
+			break;
+			default:
+				$isImg = false;
+				echo("Ошибка. Файл ".$_FILES["img"]["name"][$i]." содержит неверный формат.<br>");
+			break;
+		}
+		if (!$isImg) {
 			$i++;
+			continue;
 		}
-		echo "<br>";
-		for ($i = 0; $i < count($uploadedImgs); $i++) {
-			echo "<img src=\"../content_imgs/news_imgs/".$uploadedImgs[$i].".jpg\" width=\"75\">";
-			echo "<a target=\"_blank\" href=\"../content_imgs/news_imgs/".$uploadedImgs[$i].".jpg\">../content_imgs/news_imgs/".$uploadedImgs[$i].".jpg</a><br>";
+		$width = imagesx($img);
+		$height = imagesy($img);
+		if ($width <= 955) {
+			$new_width = $width;
+			$new_height = $height;
+		} else {
+			$new_width = 955;
+			$new_height = floor( $height * ( $new_width / $width));
 		}
-		die();
+		$tmp_img = imagecreatetruecolor($new_width, $new_height);
+		imagecopyresampled($tmp_img, $img, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+		$new_path = substr($_FILES["img"]["name"][$i], 0, strrpos($_FILES["img"]["name"][$i], "."));
+		$new_path .= time();
+		if (imagejpeg($tmp_img, "../content_imgs/news_imgs/".$new_path.".jpg", 85)) {
+			array_push($uploadedImgs, $new_path);
+			echo "Изображение ".$_FILES["img"]["name"][$i]." успешно загружено.<br>";
+		}
+		$i++;
 	}
+	echo "<br>";
+	for ($i = 0; $i < count($uploadedImgs); $i++) {
+		echo "<img src=\"../content_imgs/news_imgs/".$uploadedImgs[$i].".jpg\" width=\"75\">";
+		echo "<a target=\"_blank\" href=\"../content_imgs/news_imgs/".$uploadedImgs[$i].".jpg\">../content_imgs/news_imgs/".$uploadedImgs[$i].".jpg</a><br>";
+	}
+	die();
+}
 ?>
 <!doctype html>
 <html>
